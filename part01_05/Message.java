68a8a2e99ac15822369ea5f9f43fb9c74ab6622c
unknown <milosbviser@gmail.com>
2024-11-07 23:34:09 +0100

    Part01_06: Exercise completed

--- part01_05/Message.java
+++ part01_05/Message.java
@@ -1,5 +1,4 @@
 package part01_05;
-
 import java.util.Scanner;
 
 public class Message {
---
+++ part01_06/HiAdaLovelace.java
@@ -0,0 +1,11 @@
+package part01_06;
+
+public class HiAdaLovelace {
+
+	public static void main(String[] args) {
+		String name = "Ada Lovelace";
+		
+		System.out.println("Hi " + name + "!");
+	}
+
+}
