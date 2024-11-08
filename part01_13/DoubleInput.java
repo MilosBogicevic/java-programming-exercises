ce42c670e164391a3d123a2a66f7f82c36f8150b
unknown <milosbviser@gmail.com>
2024-11-08 23:13:21 +0100

    Part01_14: Exercise completed

--- part01_13/DoubleInput.java
+++ part01_13/DoubleInput.java
@@ -1,5 +1,4 @@
 package part01_13;
-
 import java.util.Scanner;
 
 public class DoubleInput {
---
+++ part01_14/BooleanInput.java
@@ -0,0 +1,16 @@
+package part01_14;
+
+import java.util.Scanner;
+
+public class BooleanInput {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Write something:");
+		boolean booleanInput = Boolean.valueOf(scanner.nextLine());
+		
+		System.out.println("True or false? " + booleanInput);
+	}
+
+}
