10a8cadea448ced321e116b1d66c339c9a1eec3c
unknown <milosbviser@gmail.com>
2024-11-09 17:03:41 +0100

    Part01_21: Exercise completed

---
+++ part01_21/AverageOfTwoNumbers.java
@@ -0,0 +1,24 @@
+package part01_21;
+
+import java.util.Scanner;
+
+public class AverageOfTwoNumbers {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Give the first number:");
+		int firstNumber = Integer.valueOf(scanner.nextLine());
+		
+		System.out.println("Give the second number:");
+		int secondNumber = Integer.valueOf(scanner.nextLine());
+		
+		int sum = firstNumber + secondNumber;
+		double average = (double) sum / 2.0;
+		
+		System.out.println("The average is " + average);
+		
+		scanner.close();
+	}
+
+}
