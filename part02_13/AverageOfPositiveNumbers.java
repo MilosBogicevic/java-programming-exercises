9ebe3a92dbed74d92ecda7723c909cdb66198bac
unknown <milosbviser@gmail.com>
2024-11-12 00:22:57 +0100

    Part02_13: Exercise completed

---
+++ part02_13/AverageOfPositiveNumbers.java
@@ -0,0 +1,38 @@
+package part02_13;
+
+import java.util.Scanner;
+
+public class AverageOfPositiveNumbers {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+		int count = 0;
+		int sum = 0;
+		while (true) {
+			int number = Integer.valueOf(scanner.nextLine());
+
+			if (number == 0) {
+				break;
+			}
+
+			if (number < 0) {
+				continue;
+			}
+
+			count++;
+			sum += number;
+		}
+
+		double average = (double) sum / count;
+
+		if (count == 0) {
+			System.out.println("Cannot calculate the average");
+		} else {
+			System.out.println(average);
+		}
+
+		scanner.close();
+	}
+
+}
