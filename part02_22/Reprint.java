4852bde421d80c432d156853d4992005afe8e332
unknown <milosbviser@gmail.com>
2024-11-15 00:46:25 +0100

    Part02_22: Exercise completed

---
+++ part02_22/Reprint.java
@@ -0,0 +1,23 @@
+package part02_22;
+
+import java.util.Scanner;
+
+public class Reprint {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.println("How many times?");
+		int times = Integer.valueOf(scanner.nextLine());
+
+		for (int i = 0; i < times; i++) {
+			printText();
+		}
+
+		scanner.close();
+	}
+
+	public static void printText() {
+		System.out.println("In a hole in the ground there lived a method");
+	}
+}
