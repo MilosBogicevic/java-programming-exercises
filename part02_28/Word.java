ec52c07a0677b06bbf54ab04e23a2e43e8d86b26
unknown <milosbviser@gmail.com>
2024-11-15 22:39:13 +0100

    Part02_28: Exercise completed

---
+++ part02_28/Word.java
@@ -0,0 +1,13 @@
+package part02_28;
+
+public class Word {
+
+	public static void main(String[] args) {
+		String word = word();
+		System.out.println(word);
+	}
+
+	public static String word() {
+		return "Hello, world.";
+	}
+}
