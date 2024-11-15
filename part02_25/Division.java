be196e3ff58a1aac42627052df7ac8acb47386a1
unknown <milosbviser@gmail.com>
2024-11-15 21:07:14 +0100

    Part02_25: Exercise completed

---
+++ part02_25/Division.java
@@ -0,0 +1,12 @@
+package part02_25;
+
+public class Division {
+
+	public static void main(String[] args) {
+		division(3, 5);
+	}
+
+	public static void division(int numerator, int denominator) {
+		System.out.println(1.0 * numerator / denominator);
+	}
+}
