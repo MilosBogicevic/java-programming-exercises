9fb6bb73acc42a410012b747a61c735b62b2de6e
unknown <milosbviser@gmail.com>
2024-11-18 23:02:02 +0100

    Part03_21: Exercise completed

---
+++ part03_21/PrintNeatly.java
@@ -0,0 +1,18 @@
+package part03_21;
+
+public class PrintNeatly {
+
+	public static void main(String[] args) {
+		int[] array = { 5, 1, 3, 4, 2 };
+		printNeatly(array);
+	}
+
+	public static void printNeatly(int[] array) {
+		for (int i = 0; i < array.length; i++) {
+			System.out.print(array[i]);
+			if (i < (array.length - 1)) {
+				System.out.print(", ");
+			}
+		}
+	}
+}
