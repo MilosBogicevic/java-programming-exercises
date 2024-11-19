8f9e4789061daba356cd5e81bc5b0c3bfb9701c4
unknown <milosbviser@gmail.com>
2024-11-19 23:31:03 +0100

    Part03_31: Exercise completed

--- part03_31/NameOfTheOldest.java
+++ part03_31/NameOfTheOldest.java
@@ -1,13 +1,15 @@
-package part03_30;
+package part03_31;
 
 import java.util.Scanner;
 
-public class AgeOfTheOldest {
+public class NameOfTheOldest {
 
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 
 		int oldest = 0;
+		String name = "";
+
 		while (true) {
 			String input = scanner.nextLine();
 
@@ -19,10 +21,11 @@ public class AgeOfTheOldest {
 			int age = Integer.valueOf(parts[1]);
 			if (oldest < age) {
 				oldest = age;
+				name = parts[0];
 			}
 		}
 
-		System.out.println("Age of the oldest: " + oldest);
+		System.out.println("Name of the oldest: " + name);
 
 		scanner.close();
 	}
