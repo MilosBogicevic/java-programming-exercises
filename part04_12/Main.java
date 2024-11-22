90f2c9cadc0b2b9ac74243cdf40bb430616220ef
unknown <milosbviser@gmail.com>
2024-11-22 10:14:32 +0100

    Part04_12: Exercise completed

---
+++ part04_12/Main.java
@@ -0,0 +1,18 @@
+package part04_12;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Gauge gauge = new Gauge();
+
+		while (!gauge.full()) {
+			System.out.println("Not full! Value: " + gauge.value());
+			gauge.increase();
+		}
+
+		System.out.println("Full! Value: " + gauge.value());
+		gauge.decrease();
+		System.out.println("Not full! Value: " + gauge.value());
+	}
+
+}
