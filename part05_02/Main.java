1a090e8ca4a12910fc05bd9e6b0521053ef01d63
unknown <milosbviser@gmail.com>
2024-12-01 23:56:54 +0100

    Part05_02: Exercise completed

---
+++ part05_02/Main.java
@@ -0,0 +1,10 @@
+package part05_02;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Book b = new Book("J. K. Rowling", "Harry Potter and the Sorcerer's Stone", 223);
+		System.out.println(b);
+	}
+
+}
