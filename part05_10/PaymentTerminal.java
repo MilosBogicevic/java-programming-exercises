a9dee5e27d4e620747f51f5c2a7f1fa5c5a91ae5
unknown <milosbviser@gmail.com>
2024-12-08 22:53:09 +0100

    Part05_10: Part 2 completed

--- part05_10/PaymentTerminal.java
+++ part05_10/PaymentTerminal.java
@@ -1,30 +1,37 @@
 package part05_10;
 
 public class PaymentTerminal {
-	private double money; // amount of cash
-	private int affordableMeals; // number of sold affordable meals
-	private int heartyMeals; // number of sold hearty meals
+	private double money;
+	private int affordableMeals;
+	private int heartyMeals;
 
 	public PaymentTerminal() {
-		// register initially has 1000 euros of money
+		this.money = 1000;
+		this.affordableMeals = 0;
+		this.heartyMeals = 0;
 	}
 
 	public double eatAffordably(double payment) {
-		// an affordable meal costs 2.50 euros
-		// increase the amount of cash by the price of an affordable mean and return the
-		// change
-		// if the payment parameter is not large enough, no meal is sold and the method
-		// should return the whole payment
-		return -1;
+		double mealPrice = 2.5;
+		if (payment >= mealPrice) {
+			affordableMeals++;
+			this.money += mealPrice;
+			return payment - mealPrice;
+		} else {
+			return payment;
+		}
+
 	}
 
 	public double eatHeartily(double payment) {
-		// a hearty meal costs 4.30 euros
-		// increase the amount of cash by the price of a hearty mean and return the
-		// change
-		// if the payment parameter is not large enough, no meal is sold and the method
-		// should return the whole payment
-		return -1;
+		double mealPrice = 4.3;
+		if (payment >= mealPrice) {
+			heartyMeals++;
+			this.money += mealPrice;
+			return payment - mealPrice;
+		} else {
+			return payment;
+		}
 	}
 
 	@Override
